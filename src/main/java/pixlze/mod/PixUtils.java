f34e14ba31073709878caf1dc654dbdac2785fbf
helooo1233 <71606730+helooo1233@users.noreply.github.com>
2024-08-28 01:07:56 -0400

    chore: added logging for api requests

--- src/main/java/pixlze/mod/PixUtils.java
+++ src/main/java/pixlze/mod/PixUtils.java
@@ -129,11 +129,12 @@ public class PixUtils implements ModInitializer {
                 try {
                     HttpResponse response = httpClient.execute(get);
                     wynnPlayerInfo = gson.fromJson(EntityUtils.toString(response.getEntity()), JsonObject.class);
+                    PixUtils.LOGGER.info("successfully loaded wynn player info");
                 } catch (Exception e) {
                     PixUtils.LOGGER.error("wynn player load error: {}", e.getMessage());
                 }
             } else {
-                PixUtils.LOGGER.info("null player");
+                PixUtils.LOGGER.warn("null player or already initialized wynn player info");
             }
             if (wynnPlayerInfo != null && PixUtils.guildRaidServerToken == null) {
                 HttpPost post = new HttpPost(PixUtils.secrets.get("guild_raid_urls").getAsJsonObject().get(wynnPlayerInfo.get("guild").getAsJsonObject().get("prefix").getAsString()).getAsString() + "auth/getToken");
@@ -144,13 +145,15 @@ public class PixUtils implements ModInitializer {
                     JsonObject response = gson.fromJson(EntityUtils.toString(httpClient.execute(post).getEntity()), JsonObject.class);
                     if (response.get("status").getAsBoolean()) {
                         guildRaidServerToken = response.get("token").getAsString();
-
+                        PixUtils.LOGGER.info("successfully loaded guild raid server token");
                     } else {
                         PixUtils.LOGGER.error("Couldn't generate token with error: {}", response.get("error"));
                     }
                 } catch (Exception e) {
                     PixUtils.LOGGER.error("get token error: {}", e.getMessage());
                 }
+            } else {
+                PixUtils.LOGGER.warn("wynn player info not initialized or guild raid server token already initialized");
             }
         });
 
