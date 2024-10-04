15ece2db132104d6116c2a0d4d14e9a9ef7f1bde
helooo1233 <71606730+helooo1233@users.noreply.github.com>
2024-10-03 21:04:37 -0400

    chore: remove finished todo

--- src/main/java/pixlze/guildapi/features/DiscordBridgeFeature.java
+++ src/main/java/pixlze/guildapi/features/DiscordBridgeFeature.java
@@ -29,7 +29,6 @@ public class DiscordBridgeFeature extends Feature {
     private final Pattern GUILD_PATTERN = Pattern.compile("^§[b8]((\uDAFF\uDFFC\uE006\uDAFF\uDFFF\uE002\uDAFF\uDFFE)|(\uDAFF\uDFFC\uE001\uDB00\uDC06))§[b8] (?<content>.*)$");
     private final Pattern PARTY_CONFLICT_PATTERN = Pattern.compile("^§8\uDAFF\uDFFC\uE001\uDB00\uDC06§8 [a-zA-Z0-9_]{2,16}:.*$");
 
-    // TODO filter out illegal characters here
     @Override
     public void init() {
         ClientCommandRegistrationCallback.EVENT.register((dispatcher, registryAccess) -> {
