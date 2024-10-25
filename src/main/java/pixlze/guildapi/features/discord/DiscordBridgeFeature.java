f78d094596bc16e3a5198b8385c4a03920b03232
pixlze <henrygoals2007@gmail.com>
2024-10-24 22:32:21 -0400

    fix: add some missing messages

--- src/main/java/pixlze/guildapi/features/discord/DiscordBridgeFeature.java
+++ src/main/java/pixlze/guildapi/features/discord/DiscordBridgeFeature.java
@@ -46,9 +46,10 @@ public class DiscordBridgeFeature extends Feature {
             "^Only (?<time>.+?) left to complete the Weekly Guild Objectives!$",
             // Guild member management
             "^§.(?<recruiter>.+?)§. has invited (?<recruit>.+?) to the guild$",
+            "^§.(?<recruiter>.+?)§. has uninvited (?<recruit>.+?) from the guild$",
             "^(?<recruit>.+?) has joined the guild, say hello!$",
             "^(?<username>.+?) has left the guild$",
-            "^§.(?<kicker>.+?)§. has kicked §.(?<kicked>.+?)§. from the guild$",
+            "^(§.)?(?<kicker>.+?)(§.)? has kicked (§.)?(?<kicked>.+?)(§.)? from the guild$",
             "^(?<setter>.+?) has set (?<set>.+?) guild rank from §.(?<original>\\w+)§. to §.(?<new>\\w+)$",
             // War
             "^The war for (?<territory>.+?) will start in .*$",
