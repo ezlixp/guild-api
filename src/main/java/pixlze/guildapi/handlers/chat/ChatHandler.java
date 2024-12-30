cc1354b148a6328140c805b386751925cfa02405
pixlze <henrygoals2007@gmail.com>
2024-12-30 01:00:39 -0500

    feat: greatly improve chat handler to allow duplicate messages and be more consistent

--- src/main/java/pixlze/guildapi/handlers/chat/ChatHandler.java
+++ src/main/java/pixlze/guildapi/handlers/chat/ChatHandler.java
@@ -12,7 +12,6 @@ import pixlze.guildapi.utils.text.TextUtils;
 import pixlze.guildapi.utils.text.type.TextParseOptions;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.regex.Pattern;
@@ -23,13 +22,11 @@ public final class ChatHandler extends Handler {
             Pattern.compile("^ *§[47]Press §[cf](SNEAK|SHIFT) §[47]to continue$");
     private static final Pattern NPC_SELECT_PATTERN =
             Pattern.compile("^ *§[47cf](Select|CLICK) §[47cf]an option (§[47])?to continue$");
-    private static final int CHAT_SCREEN_TICK_DELAY = 3;
+    private static final int CHAT_SCREEN_TICK_DELAY = 1;
 
     private ArrayList<Text> collectedLines = new ArrayList<>();
     private long chatScreenTicks = 0;
-    private String lastRealChat = null;
-    private String oneBeforeLastRealChat = null;
-    private String lastConfirmationlessDialogue = null;
+    private LinkedList<Text> lastCollected = new LinkedList<>();
 
     @Override
     public void init() {
@@ -37,11 +34,22 @@ public final class ChatHandler extends Handler {
         WorldStateEvents.CHANGE.register(this::onConnectionChange);
     }
 
+    private void onConnectionChange(WorldState state) {
+        GuildApi.LOGGER.info("CONNECTION CHANGE");
+        lastCollected = new LinkedList<>();
+        collectedLines = new ArrayList<>();
+        chatScreenTicks = 0;
+    }
+
     private void onWynnMessage(Text message) {
         assert McUtils.mc().world != null;
         long currentTicks = McUtils.mc().world.getTime();
 
         List<Text> lines = TextUtils.splitLines(message);
+        handleLines(lines, message, currentTicks);
+    }
+
+    private synchronized void handleLines(List<Text> lines, Text message, long currentTicks) {
         if (lines.size() > 1 || (message.getString().isEmpty() && (currentTicks <= chatScreenTicks + CHAT_SCREEN_TICK_DELAY))) {
             if (currentTicks <= chatScreenTicks + CHAT_SCREEN_TICK_DELAY) {
                 collectedLines.addAll(lines);
@@ -56,161 +64,130 @@ public final class ChatHandler extends Handler {
             if (chatScreenTicks != 0) {
                 processCollected();
             }
+            lastCollected.addLast(message);
+            while (lastCollected.size() > 5) lastCollected.removeFirst();
             postChatLine(message);
         }
     }
 
-    private void onConnectionChange(WorldState state) {
-        GuildApi.LOGGER.info("CONNECTION CHANGE");
-        lastRealChat = null;
-        oneBeforeLastRealChat = null;
-        lastConfirmationlessDialogue = null;
-        collectedLines = new ArrayList<>();
-        chatScreenTicks = 0;
-    }
-
-    private void processCollected() {
-        GuildApi.LOGGER.info("{} {} last chats", oneBeforeLastRealChat, lastRealChat);
-        List<Text> lines = new ArrayList<>(collectedLines);
-        for (Text line : lines) {
-            GuildApi.LOGGER.info("Collected line: {}", line.getString());
+    // add back last real message for first chat screen checking
+    private synchronized void processCollected() {
+        ArrayList<Text> filteredCollected = new ArrayList<>();
+        for (int i = 0; i < collectedLines.size(); i++) {
+            Text line = collectedLines.get(i);
+            filteredCollected.add(line);
+            if ((NPC_CONFIRM_PATTERN.matcher(TextUtils.parseStyled(line, TextParseOptions.DEFAULT)).find() ||
+                    NPC_SELECT_PATTERN.matcher(TextUtils.parseStyled(line, TextParseOptions.DEFAULT)).find() ||
+                    (i >= 3 &&
+                            EMPTY_LINE_PATTERN.matcher(TextUtils.parsePlain(collectedLines.get(i))).find() &&
+                            EMPTY_LINE_PATTERN.matcher(TextUtils.parsePlain(collectedLines.get(i - 1))).find() &&
+                            EMPTY_LINE_PATTERN.matcher(TextUtils.parsePlain(collectedLines.get(i - 2))).find() &&
+                            EMPTY_LINE_PATTERN.matcher(TextUtils.parsePlain(collectedLines.get(i - 3))).find()
+                    )
+            ) && i != collectedLines.size() - 1) {
+                filteredCollected.clear();
+            }
         }
-        GuildApi.LOGGER.info("Collected line spacer: \n\n\n");
 
-        collectedLines = new ArrayList<>();
-        chatScreenTicks = 0;
+        collectedLines = filteredCollected;
+        if (collectedLines.isEmpty()) return;
+        if (NPC_CONFIRM_PATTERN.matcher(TextUtils.parseStyled(collectedLines.getLast(), TextParseOptions.DEFAULT)).find()) {
+            if (collectedLines.size() < 4) {
+                GuildApi.LOGGER.warn("Unable to safely remove 4 lines for npc confirm pattern.");
+                return;
+            }
+            for (int i = 0; i < 4; i++) {
+                collectedLines.removeLast();
+            }
+        } else if (NPC_SELECT_PATTERN.matcher(TextUtils.parseStyled(collectedLines.getLast(), TextParseOptions.DEFAULT)).find()) {
+            try {
+                collectedLines.removeLast();
+                do {
+                    collectedLines.removeLast();
+                } while (!EMPTY_LINE_PATTERN.matcher(TextUtils.parseStyled(collectedLines.getLast(), TextParseOptions.DEFAULT)).find());
+                collectedLines.removeLast();
+                collectedLines.removeLast();
+                collectedLines.removeLast();
+            } catch (Exception e) {
+                GuildApi.LOGGER.warn("Unable to safely remove lines for npc select pattern.");
+                return;
+            }
+        } else if (collectedLines.size() > 4) {
+            if (EMPTY_LINE_PATTERN.matcher(TextUtils.parsePlain(collectedLines.getLast())).find() &&
+                    EMPTY_LINE_PATTERN.matcher(TextUtils.parsePlain(collectedLines.get(collectedLines.size() - 2))).find() &&
+                    EMPTY_LINE_PATTERN.matcher(TextUtils.parsePlain(collectedLines.get(collectedLines.size() - 3))).find() &&
+                    EMPTY_LINE_PATTERN.matcher(TextUtils.parsePlain(collectedLines.get(collectedLines.size() - 4))).find()
+            ) {
+                collectedLines.removeLast();
+                collectedLines.removeLast();
+                collectedLines.removeLast();
+                collectedLines.removeLast();
+            }
+        }
 
-        Collections.reverse(lines);
+        filteredCollected = new ArrayList<>();
+        for (Text line : collectedLines) {
+            if (!EMPTY_LINE_PATTERN.matcher(TextUtils.parsePlain(line)).find()) filteredCollected.add(line);
+        }
+        collectedLines = filteredCollected;
+
+        if (collectedLines.size() % 2 == 0) {
+            boolean doubled = true;
+            filteredCollected = new ArrayList<>();
+            for (int i = 0; i < collectedLines.size() / 2; i++) {
+                if (!TextUtils.parsePlain(collectedLines.get(i)).equals(TextUtils.parsePlain(collectedLines.get(i + collectedLines.size() / 2)))) {
+                    doubled = false;
+                    break;
+                }
+                filteredCollected.add(collectedLines.get(i));
+            }
+            if (doubled) {
+                collectedLines = filteredCollected;
+            }
+        }
+
+
+        for (Text line : collectedLines) {
+            GuildApi.LOGGER.info("collected: {} {}", TextUtils.parsePlain(line), TextUtils.parseStyled(line, TextParseOptions.DEFAULT));
+        }
+        GuildApi.LOGGER.info("collected spacer: \n\n\n");
+        if (collectedLines.isEmpty()) return;
 
         LinkedList<Text> newLines = new LinkedList<>();
-        if (lastRealChat == null) {
-            lines.forEach(newLines::addLast);
-        } else {
-            for (Text line : lines) {
-                String plainText = TextUtils.parsePlain(line);
-                // TODO make this not filter duplicates
-                if (plainText.equals(lastRealChat)) {
+        for (int i = 0; i < lastCollected.size(); i++) {
+            int index = 0;
+            boolean works = true;
+            while (i + index < lastCollected.size() && index < collectedLines.size()) {
+                if (!TextUtils.parsePlain(lastCollected.get(i + index)).equals(TextUtils.parsePlain(collectedLines.get(index)))) {
+                    works = false;
                     break;
                 }
-                if (plainText.equals(oneBeforeLastRealChat)) {
-                    lastRealChat = oneBeforeLastRealChat;
-                    oneBeforeLastRealChat = null;
-                    break;
+                ++index;
+            }
+            if (works) {
+                int extra = collectedLines.size() - lastCollected.size() + i;
+                for (int j = collectedLines.size() - 1; j >= collectedLines.size() - extra; j--) {
+                    newLines.addFirst(collectedLines.get(j));
                 }
-                newLines.addLast(line);
+                break;
             }
         }
-        if (newLines.isEmpty()) return;
+        lastCollected = new LinkedList<>(collectedLines);
+        processNewLines(newLines);
+    }
 
-        boolean expectedConfirmationlessDialogue = false;
-
-        if (newLines.getLast().getString().isEmpty()) {
-            if (newLines.size() == 2) {
-                if (EMPTY_LINE_PATTERN.matcher(newLines.getFirst().getString()).matches()) {
-                    return;
-                }
-
-                expectedConfirmationlessDialogue = true;
-                GuildApi.LOGGER.info("expecting confirmationless dialogue [#1]");
-            } else if (newLines.size() == 4) {
-                if (EMPTY_LINE_PATTERN.matcher(newLines.get(0).getString()).matches() &&
-                        EMPTY_LINE_PATTERN.matcher(newLines.get(1).getString()).matches() &&
-                        EMPTY_LINE_PATTERN.matcher(newLines.get(2).getString()).matches() &&
-                        EMPTY_LINE_PATTERN.matcher(newLines.get(3).getString()).matches()
-                ) {
-                    expectedConfirmationlessDialogue = true;
-                    newLines.removeFirst();
-                    newLines.removeFirst();
-                    GuildApi.LOGGER.info("expecting confirmationless dialogue [#2]");
-                }
-            }
-            newLines.removeLast();
+    private void processNewLines(LinkedList<Text> newLines) {
+        for (Text line : newLines) {
+            GuildApi.LOGGER.info("newline: {}", TextUtils.parseStyled(line, TextParseOptions.DEFAULT));
         }
-        processNewLines(newLines, expectedConfirmationlessDialogue);
+        if (!newLines.isEmpty())
+            GuildApi.LOGGER.info("newline spacer: \n\n\n");
+        for (Text line : newLines) {
+            postChatLine(line);
+        }
     }
 
     private void postChatLine(Text line) {
-        if (!TextUtils.parsePlain(line).isBlank()) {
-            oneBeforeLastRealChat = lastRealChat;
-            lastRealChat = TextUtils.parsePlain(line);
-        }
         ChatMessageReceived.EVENT.invoker().interact(line);
-    }
-
-    private void processNewLines(LinkedList<Text> newLines, boolean expectedConfirmationlessDialogue) {
-//        for (Text line : newLines) {
-//            if (!TextUtils.parseStyled(line, TextParseOptions.DEFAULT).isBlank()) {
-//                GuildApi.LOGGER.info("newline: {}", TextUtils.parseStyled(line, TextParseOptions.DEFAULT));
-//            }
-//        }
-//        GuildApi.LOGGER.info("newline spacer: \n\n\n");
-        LinkedList<Text> newChatLines = new LinkedList<>();
-
-        Text firstText = newLines.getFirst();
-        boolean isNpcConfirm = NPC_CONFIRM_PATTERN.matcher(TextUtils.parseStyled(firstText, TextParseOptions.DEFAULT))
-                .find();
-        boolean isNpcSelect = NPC_SELECT_PATTERN.matcher(TextUtils.parseStyled(firstText, TextParseOptions.DEFAULT))
-                .find();
-
-        if (isNpcConfirm || isNpcSelect) {
-            newLines.removeFirst();
-            if (newLines.isEmpty()) {
-                return;
-            }
-            if (newLines.getFirst().getString().isEmpty()) {
-                newLines.removeFirst();
-            } else {
-                GuildApi.LOGGER.warn("Malformed dialog [#1]: {}", newLines.getFirst());
-            }
-
-            boolean dialogDone = false;
-            boolean optionsFound = !isNpcSelect;
-
-            for (Text line : newLines) {
-                if (!dialogDone) {
-                    if (EMPTY_LINE_PATTERN.matcher(line.getString()).find()) {
-                        if (!optionsFound) {
-                            optionsFound = true;
-                        } else {
-                            dialogDone = true;
-                        }
-                    }
-                } else {
-                    if (!EMPTY_LINE_PATTERN.matcher(line.getString()).find()) {
-                        newChatLines.push(line);
-                    }
-                }
-            }
-        } else if (expectedConfirmationlessDialogue) {
-            if (newLines.size() != 1) {
-                GuildApi.LOGGER.warn("New lines has an unexpected dialogue count [#1]: {}", newLines);
-            }
-            lastConfirmationlessDialogue = TextUtils.parsePlain(newLines.getFirst());
-            return;
-        } else {
-            while (!newLines.isEmpty() && EMPTY_LINE_PATTERN.matcher(newLines.getFirst().getString()).find()) {
-                newLines.removeFirst();
-            }
-            Collections.reverse(newLines);
-            while (!newLines.isEmpty()) {
-                Text line = newLines.removeFirst();
-                if (EMPTY_LINE_PATTERN.matcher(line.getString()).find()) {
-                    if (newLines.isEmpty()) {
-                        break;
-                    }
-                    Text nextLine = newLines.getFirst();
-                    if (TextUtils.parsePlain(nextLine).equals(lastConfirmationlessDialogue)) {
-                        if (newLines.size() > 1) {
-                            GuildApi.LOGGER.warn("Unexpected lines after a confirmationless dialogue: {}", newLines);
-                        }
-
-                        break;
-                    }
-                }
-
-                newChatLines.addLast(line);
-            }
-        }
-        newChatLines.forEach(this::postChatLine);
     }
 }
