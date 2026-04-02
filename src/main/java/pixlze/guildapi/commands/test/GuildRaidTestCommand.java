499897c4ee9f6ed1ebd78b3e2dd8d1ab1644eae4
ezlixp <henrygoals2007@gmail.com>
2026-04-01 21:43:28 -0400

    revert: test command

--- src/main/java/pixlze/guildapi/commands/test/GuildRaidTestCommand.java
+++ src/main/java/pixlze/guildapi/commands/test/GuildRaidTestCommand.java
@@ -35,7 +35,7 @@ public class GuildRaidTestCommand extends ClientCommand {
                                                             String player2 = StringArgumentType.getString(context, "player2");
                                                             String player3 = StringArgumentType.getString(context, "player3");
                                                             String player4 = StringArgumentType.getString(context, "player4");
-                                                            Text raidFinishedMessage = Text.literal("§b\uDAFF\uDFFC\uE001\uDB00\uDC06 §e" + player1 + "§b, §e" + player2 + "§b, §e" + player3 + "§b, and §e" + player4 +
+                                                            Text raidFinishedMessage = Text.literal("§b\uDAFF\uDFFC\uE001\uDB00\uDC06§b §e" + player1 + "§b, §e" + player2 + "§b, §e" + player3 + "§b, and §e" + player4 +
                                                                     "§b finished §3" + raid + "thisisatestraid§b");
                                                             WynnChatMessage.EVENT.invoker().interact(raidFinishedMessage);
                                                             McUtils.sendLocalMessage(raidFinishedMessage, Text.empty(), false);
