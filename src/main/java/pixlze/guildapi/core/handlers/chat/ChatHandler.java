48b4279c7e00d49462f64e5d14920cc50c2e7f2e
ezlixp <henrygoals2007@gmail.com>
2026-06-13 23:01:47 -0400

    refactor: move test message

--- src/main/java/pixlze/guildapi/core/handlers/chat/ChatHandler.java
+++ src/main/java/pixlze/guildapi/core/handlers/chat/ChatHandler.java
@@ -11,31 +11,6 @@ public final class ChatHandler extends Handler {
             .append(Text.literal(" ")).append(Text.literal("Chunkywaterx rewarded ").setStyle(ColourUtils.DARK_AQUA)
                     .append(Text.literal("1024 Emeralds").setStyle(ColourUtils.YELLOW)).append(Text.literal(" to "))
                     .append(Text.literal("Doggc").setStyle(ColourUtils.DARK_AQUA)));
-    private final Text raidmessagenol31split = Text.empty().setStyle(ColourUtils.AQUA)
-            .append(Text.literal("\uDAFF\uDFFC\uE006\uDAFF\uDFFF\uE002\uDAFF\uDFFE"))
-            .append(Text.literal(" "))
-            .append(Text.literal("Pickelated").setStyle(ColourUtils.YELLOW))
-            .append(Text.literal(", "))
-            .append(Text.literal("vex710").setStyle(ColourUtils.YELLOW))
-            .append(Text.literal(", and "))
-            .append(Text.literal("Essentuan").setStyle(ColourUtils.YELLOW))
-            .append(Text.literal(" finished "))
-            .append(Text.literal("Orphion's").setStyle(ColourUtils.DARK_AQUA).append(Text.literal("\n")
-                            .append(Text.empty().setStyle(ColourUtils.AQUA.withBold(false).withUnderline(false))
-                                    .append(Text.literal("\uDAFF\uDFFC\uE001\uDB00\uDC06")).append(Text.literal(" "))))
-                    .append(Text.literal("Nexus of Light")))
-            .append(Text.literal(" and claimed "))
-            .append(Text.literal("2048x Emeralds").setStyle(ColourUtils.DARK_AQUA))
-            .append(Text.literal(", "))
-            .append(Text.literal("2x Aspects").setStyle(ColourUtils.DARK_AQUA))
-            .append(Text.literal(", "))
-            .append(Text.empty().setStyle(ColourUtils.DARK_AQUA).append(
-                    Text.literal("\n")
-                            .append(Text.empty().setStyle(ColourUtils.AQUA.withBold(false).withUnderline(false))
-                                    .append(Text.literal("\uDAFF\uDFFC\uE001\uDB00\uDC06")).append(Text.literal(" ")))
-            ).append(Text.literal("+312m Guild Experience")))
-            .append(Text.literal(", and "))
-            .append(Text.literal("+330 Seasonal Rating").setStyle(ColourUtils.DARK_AQUA));
 
     @Override
     public void init() {
